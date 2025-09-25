06c3628c22525965684755e22ec739e06e32cda4
Zhaith Izaliel <zhaith.izaliel@ethereal-edelweiss.cloud>
2025-09-25 21:53:32 +0200

    fix(package): fix sddm package to be under kdePackages instead of libsForQt5
    Closes #4

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,7 +2,8 @@
   props = builtins.fromJSON (builtins.readFile ../props.json);
 in {
   sddm-sugar-candy-nix = final: prev: {
-    sddm-sugar-candy-nix = final.libsForQt5.callPackage ./default.nix {
+    sddm-sugar-candy-nix = prev.libsForQt5.callPackage ./default.nix {
+      sddm = prev.kdePackages.sddm;
       version = props.version;
     };
   };
